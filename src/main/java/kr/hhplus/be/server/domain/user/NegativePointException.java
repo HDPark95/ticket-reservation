a08d394f93116634fdcb60640473648801008c85
HyundooPark <qkrgusen456@gmail.com>
2025-01-12 17:54:03 +0900

    feat: 유저 도메인 개발, 단위테스트
    포인트 충전, 사용 로직

---
+++ src/main/java/kr/hhplus/be/server/domain/user/NegativePointException.java
@@ -0,0 +1,7 @@
+package kr.hhplus.be.server.domain.user;
+
+public class NegativePointException extends RuntimeException {
+    public NegativePointException(String message) {
+        super(message);
+    }
+}
