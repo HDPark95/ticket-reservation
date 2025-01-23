e9dd786358e4e9b1bf4e3b3873d71b3e3bbfc936
HyundooPark <qkrgusen456@gmail.com>
2025-01-24 04:12:46 +0900

    [feat] 레디스 분산락

--- src/test/java/kr/hhplus/be/server/TestcontainersConfiguration.java
+++ src/test/java/kr/hhplus/be/server/TestcontainersConfiguration.java
@@ -5,24 +5,37 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
 @Configuration
 class TestcontainersConfiguration {
 
 	public static final MySQLContainer<?> MYSQL_CONTAINER;
+	public static final GenericContainer REDIS;
 
 	static {
 		MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
-			.withDatabaseName("hhplus")
-			.withUsername("test")
-			.withPassword("test");
+				.withDatabaseName("hhplus")
+				.withUsername("test")
+				.withPassword("test");
 		MYSQL_CONTAINER.start();
 
 		System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
 		System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
 		System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
+
+		REDIS = new GenericContainer(DockerImageName.parse("redis:latest"))
+				.withExposedPorts(6379)
+				.withCommand("redis-server", "--requirepass", "test")
+				.waitingFor(Wait.forListeningPort());
+
+		REDIS.start();
+		System.setProperty("spring.data.redis.host", REDIS.getHost());
+		System.setProperty("spring.data.redis.port", String.valueOf(REDIS.getFirstMappedPort()));
+		System.setProperty("spring.data.redis.password", "test");
 	}
 
 	@PreDestroy
